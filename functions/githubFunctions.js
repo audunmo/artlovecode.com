d9df3be4f678807db7490477530557221e92aa6f
Audun Bjørnerud Mo <audun.bjornerud.mo@finn.no>
2019-11-08 20:27:49 +0100

    feat: add github function

--- functions/githubFunctions.js
+++ functions/githubFunctions.js
@@ -1,18 +1,46 @@
-import { resolve } from 'path'
-import GitHub from 'github-api'
-import dotenv from 'dotenv'
+const resolve = require('path').resolve
+const GitHub = require('github-api')
+const dotenv = require('dotenv')
 
 dotenv.config({
   path: resolve(__dirname, '.env')
 })
 
-export default async (event = null, context = null, callback = null) => {
+const extractRepoData = ({ description, name, html_url, id }) => ({
+  description,
+  name,
+  html_url,
+  id
+})
+
+const repoFilter = repo =>
+  repo.fork === false &&
+  repo.name.match(/.+test.+/) === null &&
+  repo.private === false &&
+  (repo.description && repo.description.includes('include in portfolio'))
+
+module.exports.handler = async (
+  event = null,
+  context = null,
+  callback = null
+) => {
   const gh = new GitHub({
     token: process.env.GITHUB_TOKEN
   })
 
-  const githubProjects = await gh.getRepos()
+  const me = await gh.getUser('artlovecode')
+
+  const repos = await me.listRepos()
+
+  const body = JSON.stringify(
+    repos.data.filter(repoFilter).map(extractRepoData)
+  )
+
   return {
-    githubProjects
+    body,
+    statusCode: 200,
+    headers: {
+      'Content-type': 'application/json'
+    }
   }
 }
